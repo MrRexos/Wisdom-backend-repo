27b0bdbd4c7442a50cff671536c5985d67c13b42
Mr Rexos <hernanz.reio@gmail.com>
2024-08-25 12:55:24 +0200

    Added mysql2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 
