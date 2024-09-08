4409b6e2d2789e0da5d90615d3073926b93f00ab
MrRexos <hernanz.reio@gmail.com>
2024-09-08 19:52:39 +0200

    Trying to solve the connections error with connection limit=10 and relase connection with try finally

--- index.js
+++ index.js
@@ -12,43 +12,38 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware para parsear JSON
 app.use(bodyParser.json());
 app.use(express.json());
 
-// Configuración del pool de conexiones a la base de datos
 const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   waitForConnections: true,
-  connectionLimit: 100,  // Número máximo de conexiones en el pool
-  acquireTimeout: 10000,  // Tiempo máximo para adquirir una conexión
-  idleTimeout: 60000,     // Tiempo máximo que una conexión puede estar inactiva antes de ser liberada
+  connectionLimit: 10,  // Reducido para evitar superar el límite de Heroku
+  acquireTimeout: 10000,
+  idleTimeout: 60000,
 });
 
 const credentials = JSON.parse(process.env.GCLOUD_KEYFILE_JSON);
 
-// Configura el almacenamiento de Google Cloud
 const storage = new Storage({
   projectId: credentials.project_id,
-  credentials: credentials, 
+  credentials: credentials,
 });
 
 const bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);
 
-// Configura Multer para manejar la subida de archivos
 const multerMid = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 10 * 1024 * 1024, // Límite de 10MB por archivo
+    fileSize: 10 * 1024 * 1024,
   },
 });
 
-app.use(multerMid.single('file')); 
+app.use(multerMid.single('file'));
 
-// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('El backend está funcionando.');
 });
@@ -62,16 +57,18 @@ app.get('/api/users', (req, res) => {
       return;
     }
 
-    connection.query('SELECT * FROM user_account', (err, results) => {
-      connection.release(); // Libera la conexión después de usarla
-
-      if (err) {
-        console.error('Error al obtener usuarios:', err);
-        res.status(500).json({ error: 'Error al obtener usuarios.' });
-        return;
-      }
-      res.json(results);
-    });
+    try {
+      connection.query('SELECT * FROM user_account', (err, results) => {
+        if (err) {
+          console.error('Error al obtener usuarios:', err);
+          res.status(500).json({ error: 'Error al obtener usuarios.' });
+          return;
+        }
+        res.json(results);
+      });
+    } finally {
+      connection.release();
+    }
   });
 });
 
@@ -82,7 +79,6 @@ app.post('/api/signup', async (req, res) => {
   try {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-
     const query = 'INSERT INTO user_account (email, username, password, first_name, surname, joined_datetime, language, allow_notis, profile_picture) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?)';
     const values = [email, username, hashedPassword, first_name, surname, language, allow_notis, profile_picture];
 
@@ -93,16 +89,18 @@ app.post('/api/signup', async (req, res) => {
         return;
       }
 
-      connection.query(query, values, (err, results) => {
-        connection.release(); // Libera la conexión después de usarla
-
-        if (err) {
-          console.error('Error al crear el usuario:', err);
-          res.status(500).send('Error al crear el usuario.');
-          return;
-        }
-        res.status(201).send('Usuario creado.');
-      });
+      try {
+        connection.query(query, values, (err, results) => {
+          if (err) {
+            console.error('Error al crear el usuario:', err);
+            res.status(500).send('Error al crear el usuario.');
+            return;
+          }
+          res.status(201).send('Usuario creado.');
+        });
+      } finally {
+        connection.release();
+      }
     });
   } catch (err) {
     console.error('Error al hashear la contraseña:', err);
@@ -122,17 +120,19 @@ app.get('/api/check-email', (req, res) => {
       return;
     }
 
-    connection.query(query, [email], (err, results) => {
-      connection.release(); // Libera la conexión después de usarla
-
-      if (err) {
-        console.error('Error al verificar el email:', err);
-        res.status(500).json({ error: 'Error al verificar el email.' });
-        return;
-      }
-      const count = results[0].count;
-      res.json({ exists: count > 0 });
-    });
+    try {
+      connection.query(query, [email], (err, results) => {
+        if (err) {
+          console.error('Error al verificar el email:', err);
+          res.status(500).json({ error: 'Error al verificar el email.' });
+          return;
+        }
+        const count = results[0].count;
+        res.json({ exists: count > 0 });
+      });
+    } finally {
+      connection.release();
+    }
   });
 });
 
@@ -148,17 +148,19 @@ app.get('/api/check-username', (req, res) => {
       return;
     }
 
-    connection.query(query, [username], (err, results) => {
-      connection.release(); // Libera la conexión después de usarla
-
-      if (err) {
-        console.error('Error al verificar el nombre de usuario:', err);
-        res.status(500).json({ error: 'Error al verificar el nombre de usuario.' });
-        return;
-      }
-      const count = results[0].count;
-      res.json({ exists: count > 0 });
-    });
+    try {
+      connection.query(query, [username], (err, results) => {
+        if (err) {
+          console.error('Error al verificar el nombre de usuario:', err);
+          res.status(500).json({ error: 'Error al verificar el nombre de usuario.' });
+          return;
+        }
+        const count = results[0].count;
+        res.json({ exists: count > 0 });
+      });
+    } finally {
+      connection.release();
+    }
   });
 });
 
@@ -174,32 +176,35 @@ app.post('/api/login', (req, res) => {
       return;
     }
 
-    connection.query(query, [usernameOrEmail, usernameOrEmail], async (err, results) => {
-      connection.release(); // Libera la conexión después de usarla
-
-      if (err) {
-        console.error('Error al iniciar sesión:', err);
-        res.status(500).json({ error: 'Error al iniciar sesión.' });
-        return;
-      }
-      if (results.length > 0) {
-        const user = results[0];
-        try {
-          const match = await bcrypt.compare(password, user.password);
-          if (match) {
-            delete user.password;
-            res.json({ success: true, message: 'Inicio de sesión exitoso.', user });
-          } else {
-            res.json({ success: false, message: 'Password incorrect.' });
-          }
-        } catch (error) {
-          console.error('Error al comparar la contraseña:', error);
-          res.status(500).json({ error: 'Error al procesar la solicitud.' });
+    try {
+      connection.query(query, [usernameOrEmail, usernameOrEmail], async (err, results) => {
+        if (err) {
+          console.error('Error al iniciar sesión:', err);
+          res.status(500).json({ error: 'Error al iniciar sesión.' });
+          return;
         }
-      } else {
-        res.json({ success: null, message: "Wrong user or password." });
-      }
-    });
+
+        if (results.length > 0) {
+          const user = results[0];
+          try {
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+              delete user.password;
+              res.json({ success: true, message: 'Inicio de sesión exitoso.', user });
+            } else {
+              res.json({ success: false, message: 'Password incorrect.' });
+            }
+          } catch (error) {
+            console.error('Error al comparar la contraseña:', error);
+            res.status(500).json({ error: 'Error al procesar la solicitud.' });
+          }
+        } else {
+          res.json({ success: null, message: 'Wrong user or password.' });
+        }
+      });
+    } finally {
+      connection.release();
+    }
   });
 });
 
@@ -211,36 +216,21 @@ app.post('/api/upload-image', async (req, res, next) => {
       res.status(400).send('No se subió ningún archivo.');
       return;
     }
-    
-    // Detecta el formato de la imagen
+
     const image = sharp(req.file.buffer);
     const metadata = await image.metadata();
     let format = metadata.format;
 
-    // Procesa la imagen según el formato
     let compressedImage;
-    if (format === 'jpeg' || format === 'jpg') {
-      compressedImage = await image
-        .resize({ width: 800 })  // Ajusta el tamaño si es necesario
-        .jpeg({ quality: 80 })   // Comprime la imagen JPEG
-        .toBuffer();
+    if (['jpeg', 'jpg'].includes(format)) {
+      compressedImage = await image.resize({ width: 800 }).jpeg({ quality: 80 }).toBuffer();
     } else if (format === 'png') {
-      compressedImage = await image
-        .resize({ width: 800 })
-        .png({ quality: 60 })    // Comprime la imagen PNG
-        .toBuffer();
+      compressedImage = await image.resize({ width: 800 }).png({ quality: 60 }).toBuffer();
     } else if (format === 'webp') {
-      compressedImage = await image
-        .resize({ width: 800 })
-        .webp({ quality: 60 })   // Comprime la imagen WebP
-        .toBuffer();
+      compressedImage = await image.resize({ width: 800 }).webp({ quality: 60 }).toBuffer();
     } else if (format === 'heif') {
-      compressedImage = await image
-        .resize({ width: 800 })
-        .tiff({ quality: 60 })   // Comprime la imagen HEIC
-        .toBuffer();
+      compressedImage = await image.resize({ width: 800 }).tiff({ quality: 60 }).toBuffer();
     } else {
-      // Si el formato no es compatible, puedes devolver un error
       res.status(415).send('Formato de archivo no soportado.');
       return;
     }
@@ -273,234 +263,56 @@ app.get('/api/user/:userId/lists', (req, res) => {
       return;
     }
 
-    // Obtener las listas del usuario
-    connection.query('SELECT id, list_name FROM service_list WHERE user_id = ?', [userId], (err, lists) => {
-      if (err) {
-        console.error('Error al obtener las listas:', err);
-        res.status(500).json({ error: 'Error al obtener las listas.' });
-        connection.release();  // Libera la conexión
-        return;
-      }
+    try {
+      connection.query('SELECT id, list_name FROM service_list WHERE user_id = ?', [userId], (err, lists) => {
+        if (err) {
+          console.error('Error al obtener las listas:', err);
+          res.status(500).json({ error: 'Error al obtener las listas.' });
+          return;
+        }
 
-      // Iterar sobre las listas para obtener los detalles
-      const listsWithDetailsPromises = lists.map(list => {
-        return new Promise((resolve, reject) => {
-          connection.query('SELECT COUNT(*) as item_count FROM item_list WHERE list_id = ?', [list.id], (err, itemCountResult) => {
+        const listsWithItems = [];
+        lists.forEach((list, index) => {
+          connection.query('SELECT COUNT(*) AS itemCount FROM item_list WHERE list_id = ?', [list.id], (err, countResults) => {
             if (err) {
-              return reject(err);
+              console.error('Error al obtener el número de items:', err);
+              res.status(500).json({ error: 'Error al obtener el número de items.' });
+              return;
             }
 
-            connection.query('SELECT MAX(added_datetime) as last_item_date FROM item_list WHERE list_id = ?', [list.id], (err, lastItemDateResult) => {
+            connection.query('SELECT added_datetime FROM item_list WHERE list_id = ? ORDER BY added_datetime DESC LIMIT 1', [list.id], (err, lastItemResults) => {
               if (err) {
-                return reject(err);
+                console.error('Error al obtener la fecha del último item añadido:', err);
+                res.status(500).json({ error: 'Error al obtener la fecha del último item añadido.' });
+                return;
               }
 
-              // Obtener los tres primeros servicios (service_id) de la lista
-              connection.query('SELECT service_id FROM item_list WHERE list_id = ? ORDER BY id LIMIT 3', [list.id], (err, services) => {
-                if (err) {
-                  return reject(err);
-                }
+              const lastAddedDatetime = lastItemResults.length > 0 ? lastItemResults[0].added_datetime : null;
 
-                const servicesWithImagesPromises = services.map(service => {
-                  return new Promise((resolve, reject) => {
-                    // Obtener la primera imagen para cada service_id
-                    connection.query('SELECT image_url FROM service_image WHERE service_id = ? ORDER BY `order` LIMIT 1', [service.service_id], (err, images) => {
-                      if (err) {
-                        return reject(err);
-                      }
-
-                      resolve({
-                        service_id: service.service_id,
-                        image_url: images.length > 0 ? images[0].image_url : null // Si no hay imagen, devuelve null
-                      });
-                    });
-                  });
-                });
-
-                Promise.all(servicesWithImagesPromises)
-                  .then(servicesWithImages => {
-                    resolve({
-                      id: list.id,
-                      title: list.list_name, 
-                      item_count: itemCountResult[0].item_count,
-                      last_item_date: lastItemDateResult[0].last_item_date,
-                      services: servicesWithImages
-                    });
-                  })
-                  .catch(error => reject(error));
+              listsWithItems.push({
+                id: list.id,
+                list_name: list.list_name,
+                itemCount: countResults[0].itemCount,
+                lastAddedDatetime: lastAddedDatetime,
               });
+
+              if (index === lists.length - 1) {
+                res.json(listsWithItems);
+              }
             });
           });
         });
+
+        if (lists.length === 0) {
+          res.json(listsWithItems); // Si no hay listas, devolver un array vacío
+        }
       });
-
-      Promise.all(listsWithDetailsPromises)
-        .then(listsWithDetails => {
-          res.json(listsWithDetails);
-        })
-        .catch(error => {
-          console.error('Error al obtener detalles de las listas:', error);
-          res.status(500).json({ error: 'Error al obtener detalles de las listas.' });
-        })
-        .finally(() => {
-          connection.release(); // Libera la conexión después de completar todas las consultas
-        });
-    });
-  });
-});
-
-// Ruta para obtener todos los items de una lista por su ID
-app.get('/api/lists/:id/items', (req, res) => {
-  const { id } = req.params;
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error al obtener la conexión:', err);
-      res.status(500).json({ error: 'Error al obtener la conexión.' });
-      return;
+    } finally {
+      connection.release();
     }
-
-    // Usar una sola consulta con JOIN para obtener los ítems y datos adicionales de la tabla service, price, user_account y review
-    const query = `
-      SELECT 
-        item_list.id AS item_id, 
-        item_list.list_id, 
-        item_list.service_id, 
-        item_list.note, 
-        item_list.order, 
-        item_list.added_datetime,
-        service.service_title,
-        service.description,
-        service.service_category_id,
-        service.price_id,
-        service.postal_code,
-        service.action_rate,
-        service.user_can_ask,
-        service.user_can_consult,
-        service.price_consult,
-        service.consult_via_id,
-        service.is_individual,
-        service.service_created_datetime,
-        price.price,
-        price.price_type,
-        user_account.email,
-        user_account.username,
-        user_account.password,
-        user_account.first_name,
-        user_account.surname,
-        user_account.profile_picture,
-        user_account.joined_datetime,
-        user_account.is_professional,
-        user_account.language,
-        user_account.allow_notis,
-        user_account.currency,
-        user_account.money_in_wallet,
-        user_account.professional_started_datetime,
-        user_account.is_expert,
-        user_account.is_verified,
-        user_account.strikes_num,
-        user_account.hobbies,
-        COALESCE(review_data.review_count, 0) AS review_count,
-        COALESCE(review_data.average_rating, 0) AS average_rating
-      FROM item_list
-      JOIN service ON item_list.service_id = service.id
-      JOIN price ON service.price_id = price.id
-      JOIN user_account ON service.user_id = user_account.id
-      LEFT JOIN (
-        SELECT 
-          service_id,
-          COUNT(*) AS review_count,
-          AVG(rating) AS average_rating
-        FROM review
-        GROUP BY service_id
-      ) AS review_data ON service.id = review_data.service_id
-      WHERE item_list.list_id = ?;
-    `;
-
-
-    connection.query(query, [id], (err, itemsWithService) => {
-      connection.release(); // Liberar la conexión después de usarla
-
-      if (err) {
-        console.error('Error al obtener los ítems de la lista:', err);
-        res.status(500).json({ error: 'Error al obtener los ítems de la lista.' });
-        return;
-      }
-
-      if (itemsWithService.length > 0) {
-        res.status(200).json(itemsWithService);
-      } else {
-        res.status(404).json({ message: 'No se encontraron ítems para esta lista.' });
-      }
-    });
   });
 });
 
-app.put('/api/items/:id/note', (req, res) => {
-  const { id } = req.params;
-  const { note } = req.body;
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error al obtener la conexión:', err);
-      res.status(500).json({ error: 'Error al obtener la conexión.' });
-      return;
-    }
-
-    // Consulta para actualizar la columna 'note' en la tabla 'item_list'
-    const query = `
-      UPDATE item_list
-      SET note = ?
-      WHERE id = ?
-    `;
-
-    connection.query(query, [note, id], (err, result) => {
-      connection.release(); // Liberar la conexión después de usarla
-
-      if (err) {
-        console.error('Error al actualizar la nota del ítem:', err);
-        res.status(500).json({ error: 'Error al actualizar la nota del ítem.' });
-        return;
-      }
-
-      if (result.affectedRows > 0) {
-        res.status(200).json({ message: 'Nota actualizada con éxito.' });
-      } else {
-        res.status(404).json({ message: 'Ítem no encontrado.' });
-      }
-    });
-  });
-});
-
-app.delete('/api/lists/:id', (req, res) => {
-  const { id } = req.params;
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error al obtener la conexión:', err);
-      res.status(500).json({ error: 'Error al obtener la conexión.' });
-      return;
-    }
-
-    connection.query('DELETE FROM service_list WHERE id = ?', [id], (err, result) => {
-      connection.release(); // Libera la conexión después de usarla
-
-      if (err) {
-        console.error('Error al eliminar la lista:', err);
-        res.status(500).json({ error: 'Error al eliminar la lista.' });
-        return;
-      }
-
-      if (result.affectedRows > 0) {
-        res.status(200).json({ message: 'Lista eliminada con éxito' });
-      } else {
-        res.status(404).json({ message: 'Lista no encontrada' });
-      }
-    });
-  });
-});
-
-// Inicia el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+  console.log(`Servidor en funcionamiento en el puerto ${port}`);
 });
