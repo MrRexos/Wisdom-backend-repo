ce7c42b2a5ef4f4e73124cb9f3d35bdf79719c36
MrRexos <hernanz.reio@gmail.com>
2024-12-06 00:01:28 +0100

    Solving 5

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 // Configuración del pool de conexiones a la base de datos a
 const pool = mysql.createPool({
   socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+  host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
