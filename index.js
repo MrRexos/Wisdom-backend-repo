4f49669f53ae9d792ce6cfe1b1f78ae34f10c985
Mr Rexos <hernanz.reio@gmail.com>
2025-08-07 11:36:39 +0200

    changed size photo of dni

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const upload = multer({ storage: multer.memoryStorage() });
 const uploadDni = multer({
   dest: os.tmpdir(),
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
       cb(null, true);
